8381667676eaf34a7dbfb77fe3ca186a902c147b
Liam Forsey <98509939+lforsey@users.noreply.github.com>
2023-04-17 13:34:30 -0700

    Removed duplicate logic

--- app.js
+++ app.js
@@ -4,23 +4,6 @@ const gridSize = document.querySelector('#gridSizeInput');
 // Create default grid fragment
 const frag = document.createDocumentFragment('div');
 
-const defaultGridSize = 16;
-for (let i = 0; i < defaultGridSize; i++) { //create grid rows
-    const newColumn = document.createElement('div');
-    newColumn.classList.add('default');
-    newColumn.style.height = `${Math.floor(800 / defaultGridSize)}px`;
-    for (let j = 0; j < defaultGridSize; j++) { // populate with grid squares
-        const newSquare = document.createElement('div');
-        newSquare.classList.add('square');
-        newSquare.style.width = `${Math.floor(800 / defaultGridSize)}px`;
-        newSquare.style.height = `${Math.floor(800 / defaultGridSize)}px`;
-        newColumn.appendChild(newSquare); //append square to column
-    }
-    frag.appendChild(newColumn); // append column to document fragment.
-}
-
-container.appendChild(frag); // append fragment to document.
-
 const createGrid = (size, fragment, destination) => {
     while (destination.firstChild) {
         destination.removeChild(destination.firstChild);
@@ -39,6 +22,9 @@ const createGrid = (size, fragment, destination) => {
     }
     destination.appendChild(fragment);
 }
+
+// Initialize default 16*16 grid
+createGrid(16, frag, container);
 
 gridSize.addEventListener('submit', (e) => {
     e.preventDefault();
